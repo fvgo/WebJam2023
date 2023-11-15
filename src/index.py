7b2c0781ee337e0b3a1b6bad3bab402dd4c0fe5b
Francine Go <fvgo@uci.edu>
2023-11-14 16:16:52 -0800

    Moved static and template directories outside src directory; imported render_template in index.py

--- src/index.py
+++ src/index.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    # return "<p>This is the HOME page!</p>"
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    return "<p>This is the ABOUT page!</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
