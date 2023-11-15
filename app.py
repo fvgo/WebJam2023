d387b50c8a130da7d2423b227443faa409060008
Francine Go <fvgo@uci.edu>
2023-11-14 20:05:16 -0800

    u

--- app.py
+++ app.py
@@ -2,14 +2,22 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     # return "<p>This is the HOME page!</p>"
-    return render_template("index.html", title=""hello)
+    # API CALL  --> title_variable ????
+    return render_template("index.html", title_variable="hello")
 
 @app.route("/about")
 def about():
-    return "<p>This is the ABOUT page!</p>"
+    return render_template("about.html")
+    # return "<p>This is the ABOUT page!</p>"
+
+@app.route("/courses")
+def courses():
+    return render_template("courses.html")
+    # return "<p>This is the COURSES page!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
