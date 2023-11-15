925d6847e681b5e2efde674547bf515e4bb76af9
Kevin Mach <kmach3@uci.edu>
2023-11-14 19:22:30 -0800

    Build testing environment with Jinja control flow

--- src/index.py
+++ src/index.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+# Things needed to render the current test to work
+# request is essentially a global variable that has any
+# values from a request given from the connection.
+# Flask is the actual application which is used to run
+# render_template builds the website including any
+# Jinja code that was used to make templates
+
+port = 3123
+TEMPLATE_FOLDER = "templates"
+
+
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route(f"/")
+def home():
+    return render_template("index.html")
+
+@app.route(f"/test_if", methods=["GET", "POST"])
+def test_if_statement():
+    return render_template("test_if.html", request.form["value"])
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", port=port, debug=True)
